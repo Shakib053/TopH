090f700b5bb1136e1383dae5960f72c7ec691267
Shakib053 <75584294+Shakib053@users.noreply.github.com>
2021-03-25 02:51:08 +0600

    a

---
+++ Area.cpp
@@ -0,0 +1,25 @@
+
+#include<bits/stdc++.h>
+#include<map>
+#include<iterator>
+#define pi 3.1416
+using namespace std;
+int main()
+{   
+   
+    
+    int a;
+    long long b;
+    int i=1;
+    cin>>a;
+   while(a--)
+   {
+       cin>>b;
+      
+       cout<<"Case "<<i<<": "<<2*b-4<<endl;
+       ++i;
+   }
+
+    return 0;
+
+}
---
+++ PieAreSquared.cpp
@@ -0,0 +1,16 @@
+
+#include<bits/stdc++.h>
+#include<map>
+#include<iterator>
+#define pi 3.1416
+using namespace std;
+int main()
+{   
+    double r;
+    cin>>r;
+    cout<<fixed<<setprecision(4)<<pi*pow(r,2);
+   
+    
+    return 0;
+
+}
