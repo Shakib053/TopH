d37f608d43e27adf2c35549ea399fa09a5dd38d1
Kazi Tanjim Shakib <75584294+Shakib053@users.noreply.github.com>
2021-04-28 17:24:04 +0600

    EWU In-House Programming Battle Spring 2021

---
+++ D_The CatandMouseJuxtaposition.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+#include<iterator>
+#include<string>
+#define ll long long
+using namespace std;
+int main()
+{
+int n,a,b;
+cin>>n>>a>>b;
+if(abs(a-b)%2==0)
+cout<<"YES\n";
+else
+cout<<"NO\n";
+
+return 0;
+}
